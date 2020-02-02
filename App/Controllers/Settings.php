4755874f4c456a98c2bed6f7be790fee971502a0
Mateusz Paczwa <mateusz.paczwa.programista@gmail.com>
2020-02-02 20:00:07 +0100

    Loading categories in a view of settings

--- App/Controllers/Settings.php
+++ App/Controllers/Settings.php
@@ -5,6 +5,9 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Flash;
 use \App\Models\User;
+use \App\Models\IncomeCategory;
+use \App\Models\PayingMethods;
+use \App\Models\ExpenseCategory;
 /**
  * AddIncome controller
  *
@@ -20,7 +23,16 @@ class Settings extends \Core\Controller
      */
     public function newAction()
     {
-        View::renderTemplate('Settings/new.html');
+		$payingMethods = new PayingMethods();
+		$payingMethods = PayingMethods::findByID($_SESSION['user_id']);
+		
+		$expenseCategories = new ExpenseCategory();
+		$expenseCategories = ExpenseCategory::findByID($_SESSION['user_id']);
+		
+		$incomeCategories = new IncomeCategory();
+		$incomeCategories = IncomeCategory::findByID($_SESSION['user_id']);
+		
+        View::renderTemplate('Settings/new.html', array('payingMethods' =>$payingMethods, 'expenseCategories' =>$expenseCategories, 'incomeCategories' => $incomeCategories));
     }
 
 	public function changePersonalInfoAction()
@@ -33,7 +45,7 @@ class Settings extends \Core\Controller
 
         if ($user->changePersonalInfo($name, $email, $password)) {
 			Flash::addMessage('Udało się zmienić dane użytkownika');
-			View::renderTemplate('Settings/new.html');
+			$this->newAction();
 			 
         } else {
 			Flash::addMessage('Nie udało się zmienić danych użytkowaika, proszę ponowić próbę',Flash::WARNING);
